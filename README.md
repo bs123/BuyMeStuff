22f82d54a326d903ff9ab2d14e30475ecae635ed
Your Name <you@example.com>
2017-05-13 14:15:37 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # BuyMeStuff
 7hack 2017 FE
+
+
+
+https://facebook.github.io/react-native/docs/getting-started.html#content
+
+npm i
+conntect device or start emulater (at least api-level 23.1)
+npm start
+react-native run-android (once to transfer the apk)
+
+control +m / enable hot reloding
+
+start conding
+
+
--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class BuyMeStuff extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Danke Tobi!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
